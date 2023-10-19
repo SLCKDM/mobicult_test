bd40e3e630c718a257d061c1b654d03ec1131cc6
SLCKDM <watermzfkmelon@gmail.com>
2023-10-19 23:29:35 +0300

    currency comment edit

--- app/schemes/currency.py
+++ app/schemes/currency.py
@@ -6,8 +6,7 @@ from pydantic import BaseModel
 class TunedModel(BaseModel):
 
     class Config:
-        """ Tells pydantic to convert even not dict obj to json """
-        from_attributes = True
+        from_attributes = True  # Tells pydantic to convert even not dict obj to json
 
 
 class CurrencyScheme(TunedModel):
