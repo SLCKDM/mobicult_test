60a88afd9387542f6afe962bee89be587f0e8c54
SLCKDM <watermzfkmelon@gmail.com>
2023-10-20 18:54:01 +0300

    set upd

--- app/settings.py
+++ app/settings.py
@@ -5,3 +5,4 @@ TEMPLATE_DIR = APP_DIR / "templates"
 STATIC_DIR = APP_DIR / "static"
 STATIC_URL = "/static"
 DB_URL = f'sqlite+aiosqlite:///{APP_DIR}/app.db'
+TEST_DB_URL = f'sqlite+aiosqlite:///{APP_DIR}/test_app.db'
